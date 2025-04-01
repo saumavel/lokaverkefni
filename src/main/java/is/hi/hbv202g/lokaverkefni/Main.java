50dd45382dcec36e94a624f141c3b6b28f6f08b0
saumavel <saumavel@gmail.com>
2025-04-01 11:11:50 +0000

    tesse baa buinN?

--- src/main/java/is/hi/hbv202g/lokaverkefni/Main.java
+++ src/main/java/is/hi/hbv202g/lokaverkefni/Main.java
@@ -2,6 +2,13 @@ package is.hi.hbv202g.lokaverkefni;
 
 public class Main {
     public static void main(String[] args) {
-        RockPaperScissorsGame game = new RockPaperScissorsGame();
+        Player player1 = new Player("Prump");
+        Player player2 = new Player("Piss");
+        RockPaperScissorsGame game = new RockPaperScissorsGame(player1.getName(), player2.getName());
+
+        game.playerOneSelectsRock();
+        game.playerTwoSelectsPaper();
+
+        System.out.println(game.determineWinner());
     }
 }
